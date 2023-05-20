460ff1d93e8665aa72bd9ade96926556b5d05cff
MarceloFracassi <fracassimarcelo@gmail.com>
2023-05-20 10:24:42 -0300

    Avance 1.1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import NavbarPizza from "./Components/Nav/Navbar";
+import CardPizza from './Components/PizzaCard/Card';
+
+function App() {
+
+  return (
+    <>
+      <NavbarPizza/>
+      <CardPizza/>
+    </>
+  )
+}
+
+export default App;
