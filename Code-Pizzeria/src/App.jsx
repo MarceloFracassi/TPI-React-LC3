412e57656bb6a26d3e9c44ef96a8c9811387cd36
Juan Ignacio Pagnanini <juanipagnanin@gmail.com>
2023-06-03 12:42:55 -0300

    avance 3

--- Code-Pizzeria/src/App.jsx
+++ Code-Pizzeria/src/App.jsx
@@ -1,30 +1,16 @@
 
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './Components/Home/Home';
-import NavbarPizza from "./Components/Nav/Navbar";
-import Menu from './Components/Menu/Menu';
-import History from './Components/History/History';
-import Contact from './Components/Contact/Contact';
-import Cart from './Components/Cart/cart';
+import { Navbar } from './Components/Navbar/Navbar'
+import {History} from './Components/History/History';
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <NavbarPizza />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart/>} /> 
-      </Routes>
-    </BrowserRouter>
-      {/* <NavbarPizza/> */}
-      
-      {/* <Home/> */}
+     <Navbar/>
+     <Home/>
+     <History/>
     </>
   )
 }
