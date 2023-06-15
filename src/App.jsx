b06ca68dc7bd8162c47f37672ab8a53311eeb797
MarceloFracassi <fracassimarcelo@gmail.com>
2023-06-14 22:35:14 -0300

    Avance 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import { Home } from './Components/Home/Home';
-import { Navbar } from './Components/Navbar/Navbar'
-import {History} from './Components/History/History';
+import { Navbar } from './Components/Navbar/Navbar';
+import { History } from './Components/History/History';
 import { ThemeProvider } from './Components/Context/ThemeContext';
 import ThemeButton from './Components/Context/ThemeButton';
 import Footer from './Components/Footer/Footer';
-
+import Login from './Components/Log/Login';
+import Signin from './Components/Log/Signin';
+import Cart from './Components/Cart/Cart';
+import Menu from './Components/Menu/Menu';
 
 function App() {
-
   return (
     <ThemeProvider>
-      <ThemeButton/>
-      <Navbar/>
-      <Home/>
-      <History/>
-      <Footer/>
+      <BrowserRouter>
+        <ThemeButton />
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<History />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
---
+++ src/Components/Log/Signin.jsx
@@ -0,0 +1,10 @@
+const Signin = () => {
+    return (
+      <div>
+        <h1>Log de Marcello & Santino</h1>
+
+      </div>
+    );
+  };
+  
+  export default Signin;
